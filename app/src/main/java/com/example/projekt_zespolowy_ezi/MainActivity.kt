d99ccac33a125f620f13da665ecfcf013b1d170d
Bartosz Jakubski <proallone14@gmail.com>
2021-03-21 13:35:18 +0100

    Add background gradient animation

--- app/src/main/java/com/example/projekt_zespolowy_ezi/MainActivity.kt
+++ app/src/main/java/com/example/projekt_zespolowy_ezi/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.projekt_zespolowy_ezi
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RelativeLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /* ANIMACJA TLA */
+        val layout: RelativeLayout = findViewById(R.id.layout_main)
+        val animationDrawable = layout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(4000)
+        animationDrawable.setExitFadeDuration(4000)
+        animationDrawable.start()
     }
 }
