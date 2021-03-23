dab30e984eba98e3a67052ed08228b5eb1011f45
Bartosz Jakubski <proallone14@gmail.com>
2021-03-23 16:03:26 +0100

    add splash welcome screen

---
+++ app/src/main/java/com/example/projekt_zespolowy_ezi/WelcomeView.kt
@@ -0,0 +1,40 @@
+package com.example.projekt_zespolowy_ezi
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+//REF https://www.geeksforgeeks.org/how-to-create-an-animated-splash-screen-in-android/
+
+@Suppress("DEPRECATION")
+class WelcomeView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_view)
+
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // HERE WE ARE TAKING THE REFERENCE OF OUR IMAGE
+        // SO THAT WE CAN PERFORM ANIMATION USING THAT IMAGE
+        val backgroundImage: ImageView = findViewById(R.id.WelcomeViewImage)
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide)
+        backgroundImage.startAnimation(slideAnimation)
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+    }
+}
